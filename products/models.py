b182d8dda5d66871cc96899d7d1aa2cfbed6ff84
paul6ix <okporp@gmail.com>
2022-01-15 02:44:42 +0100

    Updated the models

--- products/models.py
+++ products/models.py
@@ -19,4 +19,20 @@ class Category(models.Model):
 
 
 class Card(models.Model):
-    category = models
+    category = models.ForeignKey(Category, related_name='cards', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    slug = models.SlugField
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='uploads', blank=True, null=True)
+    thumbnail = models.ImageField(upload_to='uploads', blank=True, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-date_created',)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'{self.category.slug}/{self.slug}/'
